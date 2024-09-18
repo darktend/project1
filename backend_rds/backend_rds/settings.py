4c576e08553dded55671f0ac4809c90966074b53
vgodun <vgodunsoftware@gmail.com>
2024-09-18 12:32:26 +0300

    fix pipeline

--- backend_rds/backend_rds/settings.py
+++ backend_rds/backend_rds/settings.py
@@ -51,7 +51,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+CORS_ALLOWED_ORIGINS = CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://0.0.0.0",
+]
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
